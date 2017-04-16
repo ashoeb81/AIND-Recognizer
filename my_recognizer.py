4a1788b762a306b6410b952f7bc94c1c8ac13c7a
Ali Shoeb <ashoeb81@gmail.com>
2017-04-16 07:54:07 -0700

    Started recognizer implementation.

--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,19 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
+    for test_word_idx, test_word_features in test_set.get_all_Xlengths().items():
+        test_word = test_set.wordlist[test_word_idx]
+        scores = []
+        for word, model in models.items():
+            try:
+                logL = model.score(test_word_features[0], test_word_features[1])
+                scores.append((word, logL))
+            except:
+                continue
+        print('Test word {}'.format(test_word))
+        import pdb
+        pdb.set_trace()
+
     # TODO implement the recognizer
     # return probabilities, guesses
     raise NotImplementedError
